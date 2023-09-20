7d0f2a3bbe649b582a47ba6843fadecce33ceb33
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2023-09-20 16:48:57 -0300

    Init

---
+++ MoreApps/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MoreApps
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MoreApps/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MoreApps
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
